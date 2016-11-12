866647c2475ca207cd08ade4f573fd2308ee0f78
Razvan <razvan@yahoo.com>
2016-11-12 14:24:22 +0200

    Exercitiul 4

--- main.cpp
+++ main.cpp
@@ -2,31 +2,26 @@
 
 using namespace std;
 
-int caut(double[], int , double );
+void initial(double [], int);
 
 int main(){
-    double a[20];int  nr, i;
-    cout << "Numarul de elemente din sirul a (maximum 20) ";
-    cin >> nr;
-    for (i = 0; i < nr; i++) {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
-    }
+    double a[100];int  nr, i;
+    cout << "Numarul de elemente (mai mic sau egal cu 100): ";
+   cin >> nr;
 
-    int rez=caut(a,nr,8);
-    if(rez<0)
-        cout<<"Valoarea nu a fost gasita";
-    else
-    cout << "Valoarea introdusa se afla pe pozitia"<< rez+1 << endl;
+
+     initial(a, nr);
+
+   for (i = 0; i < nr; i++) {
+        cout << a[i] << ", ";
+    }
     return 0;
 }
 
 
-int caut(double a[], int n, double valoare){
-   int i;
-   for(i=0; i<n; i++) {
-      if(a[i] == valoare)
-	     return i;
+void initial(double a[], int n)
+{
+   for (int i = 0; i < n; i+2) {
+      a[i] = i + 1;
    }
-   return -1;  //  Nu s-a gasit
 }
