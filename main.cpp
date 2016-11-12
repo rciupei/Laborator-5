7922726e2a409f5afe2d3acb32c4f84b67580a64
Razvan <razvan@yahoo.com>
2016-11-12 13:58:12 +0200

    Exercitiul 2

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
-#include <cctype>
-#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    char rasp, varianta;
-    double x;
-    rasp = 'd';
-    while (tolower(rasp) == 'd')
-    {
-        cout << "Unghiul: "; cin >> x;
-        cout << "Ce doriti sa calculati : (s - sin, c - cos, t - tan) : ";
-        cin >> varianta;
-        switch (varianta) {
-        case 's' :
-            cout << "sin = " << sin(3.14159 * x / 180.) << endl;
-            break;
-        case 'c' :
-            cout << "cos = " << cos(3.14159 * x / 180.) << endl;
-            break;
-        case 't' :
-            cout << "tan = " << sin(3.14159 * x / 180.)/
-                                cos(3.14159 * x / 180.) << endl;
-            break;
-        default:
-            cout << "Caracter incorect!";
-        }
-        cout << "Mai continuati? (d/n) : ";
-        cin >> rasp;
-        cin.ignore(15,'\n');
+void schimba(int [], int);
+int minim(int [], int);
+
+int main(){
+    int a[20], nr, i;
+    cout << "Numarul de elemente din sirul a (maximum 20) ";
+    cin >> nr;
+    for (i = 0; i < nr; i++) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
     }
+    schimba(a, nr);   //  In sir sunt 7 elemente
+    int mini = minim(a, nr);
+    cout << "Minimul valorilor dupa schimbarea semnelor este: " << mini << endl;
     return 0;
+}
+void schimba(int a[], int n) {
+   int i;
+   for (i = 0; i < n; i++) {
+      if(a[i] < 0)
+         a[i] = -a[i];
+   }
+}
+
+int minim(int a[], int n) {
+   int i;
+   int m = a[0];
+   for (i = 1; i < n; i++) {
+      if(a[i] < m)
+         m = a[i];
+   }
+   return m;
 }
